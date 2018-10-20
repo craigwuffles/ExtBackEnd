396941310c1b68b7c1b0fde6301a74b59127ae16
craigwuffles <jaxamme@gmail.com>
2018-10-19 21:38:22 -0500

    ID

--- backend.js
+++ backend.js
@@ -102,8 +102,8 @@ ext.
   option('-l, --is-local', 'Developer rig local mode').
   parse(process.argv);
 
-const ownerId = getOption('ownerId', 'p_a_i_r', '100000001');
-const secret = Buffer.from(getOption('secret', 'C:\Users\jamme\Documents\Extension\Backend\ExtBackEnd', 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'), 'base64');
+const ownerId = getOption('ownerId', 'ENV_OWNER_ID', '222147546');
+const secret = Buffer.from(getOption('secret', 'ENV_SECRET', 'qUH1nVSRo2/QOqSJu+ucyygITprlp5UEShkVrGfotzk='), 'base64');
 let clientId;
 if (ext.isLocal && ext.args.length) {
   const localFileLocation = path.resolve(ext.args[0]);
